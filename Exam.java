7fe402cd129f9fbfb576df601462e73161f79b92
MuhammadFarazNiaz <98203095+MuhammadFarazNiaz@users.noreply.github.com>
2023-01-17 10:25:10 +0500

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -1,29 +1,11 @@
 
-package q;
-
-import java.util.LinkedList;
-import java.util.Queue;
+package Faraz;
 
 public class Exam {
 
     public static void main(String[] args) {
-        
-        Queue <Integer>q = new LinkedList<Integer>();
-           for(int i =0; i<5;i++){
-              q.add(i);}
-        System.out.println("Elements of queue are : "+q);      
-        int removedelement = q.remove();
-        
-        System.out.println("removedelement "+ removedelement );
-        
-        System.out.println("updated queue  "+ q );
-        
-        int head = q.peek();
-        
-        System.out.println("head of queue : "+head);
-        
-        int size = q.size();
-        
-        System.out.println("size of queue : "+size); 
+       
+        System.out.println("This is the main branch "); 
+        System.out.println("This is the V3 branch "); 
     }   
 }
